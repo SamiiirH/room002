cea3b82c699b2c26d078ce7019151db440feb3f4
SamiiirH <134177100+SamiiirH@users.noreply.github.com>
2023-05-30 23:46:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const doorButton = document.getElementById('doorButton');
 const doorSound = document.getElementById('doorSound');
 
 doorButton.addEventListener('click', function() {
-  doorSound.play();
-});    
+  doorSound.currentTime = 0;
+  doorSound.play().catch(function(error) {
+    console.error('Failed to play audio:', error);
+  });
+});
